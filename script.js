dfa1fe99cee377441fc20450fcb46db6089efc6e
TTmikaD <101580923+TTmikaD@users.noreply.github.com>
2023-05-08 11:13:05 +0700

    1 file modified

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log('Hello!');
+// console.log('Hello!');
+
+
+let userName = prompt('What is the “official” name of JavaScript?');
+
+if (userName == 'ECMAScript') {
+  alert('right');
+} else {
+  alert('Didn’t know? ECMAScript!');
+}
